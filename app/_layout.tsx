c15ac684f3d47a83e82d24754c8ec7a8466a563e
tigaweb <nka.webtech.dev@gmail.com>
2025-04-29 13:01:59 +0900

    ヘッダーを表示しないよう修正

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,10 @@
 import { Stack } from "expo-router";
 
-export default function RootLayout() {
+export default function Layout() {
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="+not-found" />
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="(tabs)" />
+      <Stack.Screen name="session" />
     </Stack>
   );
 }
